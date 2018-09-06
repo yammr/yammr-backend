467081f951890919eeff02d3c7f587742c419f19
FrowntownPitt <ajf93@pitt.edu>
2018-09-06 13:35:42 -0400

    Bootstrap Spring Boot with Gradle

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# GibGab Backend
+
+An anonymous social platform for posting text, pictures, and "vine-style" videos, exclusively for college students. This repository houses the Spring Boot back-end that serves the front-end. 
+
+This project was bootstrapped with [Spring Initializr](start.spring.io) by IntelliJ IDEA.
---
+++ src/main/java/com/gibgab/service/GibgabServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gibgab.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibgabServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GibgabServiceApplication.class, args);
+    }
+}
