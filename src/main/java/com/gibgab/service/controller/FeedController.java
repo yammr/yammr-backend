d15ca0653274a4f38bbce87f503b3944286bf7ae
Andrew Lobos <andrew@lobos.me>
2018-11-18 16:23:38 -0500

    Use @Value

--- src/main/java/com/gibgab/service/controller/FeedController.java
+++ src/main/java/com/gibgab/service/controller/FeedController.java
@@ -5,6 +5,7 @@ import com.gibgab.service.database.entity.Post;
 import com.gibgab.service.database.repository.PostRepository;
 import com.gibgab.service.database.repository.UserRepository;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.PageRequest;
@@ -23,15 +24,16 @@ public class FeedController {
     @Autowired
     private PostRepository postRepository;
 
+    @Value("#{'${feed.page-limit}'}")
+    private int pageLimit;
+
     @GetMapping({"/feed"})
     public @ResponseBody List<Post> get_feed() {
-        int pageLimit = Integer.parseInt(env.getRequiredProperty("feed.page-limit"));
         return postRepository.findByOrderByIdDesc(PageRequest.of(0, pageLimit));
     }
     
     @GetMapping("/feed/{page}")
     public @ResponseBody List<Post> get_feed(@PathVariable int page) {
-        int pageLimit = Integer.parseInt(env.getRequiredProperty("feed.page-limit"));
         return postRepository.findByOrderByIdDesc(PageRequest.of(page, pageLimit));
     }
 }
