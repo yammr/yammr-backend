7e315fa3bea72c5b41f93ae4432d23edb72777df
Andrew Lobos <andrew@lobos.me>
2018-11-17 20:50:12 -0500

    Add feed API

---
+++ src/main/java/com/gibgab/service/controller/FeedController.java
@@ -0,0 +1,37 @@
+package com.gibgab.service.controller;
+
+import com.gibgab.service.database.entity.ApplicationUser;
+import com.gibgab.service.database.entity.Post;
+import com.gibgab.service.database.repository.PostRepository;
+import com.gibgab.service.database.repository.UserRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.List;
+
+@RestController
+public class FeedController {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    final int POSTS_PER_PAGE = 10;
+
+    @GetMapping({"/feed"})
+    public @ResponseBody List<Post> get_feed(@PathVariable Optional<Integer> page) {
+        return postRepository.findByOrderByIdDesc(PageRequest.of(0, POSTS_PER_PAGE));
+    }
+    
+    @GetMapping("/feed/{page}")
+    public @ResponseBody List<Post> get_feed(@PathVariable int page) {
+        return postRepository.findByOrderByIdDesc(PageRequest.of(page, POSTS_PER_PAGE));
+    }
+}
--- src/main/java/com/gibgab/service/database/repository/PostRepository.java
+++ src/main/java/com/gibgab/service/database/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.gibgab.service.database.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
 import com.gibgab.service.database.entity.Post;
+
+import java.util.List;
 
 
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    List<Post> findByOrderByIdDesc(Pageable page);
 }
