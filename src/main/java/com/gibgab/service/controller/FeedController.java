b302c75e5b6715d839b681ad65d357c320cdf1df
Andrew Lobos <andrew@lobos.me>
2018-12-02 18:13:32 -0500

    Transform feed posts to format required by frontend

--- src/main/java/com/gibgab/service/controller/FeedController.java
+++ src/main/java/com/gibgab/service/controller/FeedController.java
@@ -4,7 +4,7 @@ import com.gibgab.service.database.entity.ApplicationUser;
 import com.gibgab.service.database.entity.Post;
 import com.gibgab.service.database.repository.PostRepository;
 import com.gibgab.service.database.repository.UserRepository;
-import lombok.Data;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -12,11 +12,30 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.List;
 
 @RestController
 public class FeedController {
+
+    @Data
+    public static class CommentItem {
+
+    }
+
+    @Data
+    public static class FeedItem {
+        int postId;
+        String text;
+        int score;
+        String voteType;
+        String authorName;
+        String authorPictureUrl;
+        String imageUrl;
+        int replyCount;
+        List<CommentItem> comments;
+    }
 
     @Autowired
     private Environment env;
@@ -28,15 +47,34 @@ public class FeedController {
     private int pageLimit;
 
     @GetMapping({"/feed"})
-    public @ResponseBody List<Post> get_feed_from_index(@RequestParam("start") Optional<Integer> start_id) {
+    public @ResponseBody List<FeedItem> get_feed_from_index(@RequestParam("start") Optional<Integer> start_id) {
         if ( start_id.isPresent() )
-            return postRepository.findByIdGreaterThanOrderByIdDesc(start_id.get(), PageRequest.of(0, pageLimit));
+            return transformPosts(postRepository.findByIdGreaterThanOrderByIdDesc(start_id.get(), PageRequest.of(0, pageLimit)));
         else
-            return postRepository.findByOrderByIdDesc(PageRequest.of(0, pageLimit));
+            return transformPosts(postRepository.findByOrderByIdDesc(PageRequest.of(0, pageLimit)));
     }
 
     @GetMapping("/feed/{page}")
-    public @ResponseBody List<Post> get_feed(@PathVariable int page) {
-        return postRepository.findByOrderByIdDesc(PageRequest.of(page, pageLimit));
+    public @ResponseBody List<FeedItem> get_feed(@PathVariable int page) {
+        return transformPosts(postRepository.findByOrderByIdDesc(PageRequest.of(page, pageLimit)));
+    }
+
+    private List<FeedItem> transformPosts(List<Post> posts) {
+        List<FeedItem> feed = new ArrayList<FeedItem>(posts.size());
+        for ( Post post : posts ) {
+            FeedItem item = new FeedItem();
+            item.setPostId(post.getId());
+            item.setText(post.getText());
+            item.setScore(0);
+            item.setVoteType(null);
+            item.setAuthorName(post.getAuthorName());
+            item.setAuthorPictureUrl(null);
+            item.setImageUrl(null);
+            item.setReplyCount(0);
+            item.setComments(new ArrayList<CommentItem>());
+
+            feed.add(item);
+        }
+        return feed;
     }
 }
